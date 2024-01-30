9aeb7cf43b1fa642243c1ccc4310092ac6147f28
Truecoderalert <141266977+Truecoderalert@users.noreply.github.com>
2024-01-30 15:17:04 -0500

    Add files via upload

---
+++ liking-likes/likinglikes.js
@@ -0,0 +1,35 @@
+var likes = 1;
+console.log(likes)
+
+
+function addlike(){
+    var likecount = document.querySelector(".likebtn")
+    likes++;
+    likecount.innerText=likes
+}
+
+
+var likesB =9;
+function addlikeB(){
+var likecountB = document.querySelector(".likebtnB")
+likesB++;
+likecountB.innerText=likesB
+
+}
+
+var likesC = 12
+function addlikeC(){
+var likecountC = document.querySelector(".likebtnC")
+likesC++;
+likecountC.innerText=likesC
+
+}
+
+    var likesD = 9
+    function addlikeD(){
+var likecountD = document.querySelector(".likebtnD")
+likesD++;
+likecountD.innerText=likesD
+
+
+    }
